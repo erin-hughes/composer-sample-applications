99cb9bea98baa56b2f52d28e0d107f55205cccb5
Erin Hughes <Erin.Hughes@ibm.com>
2018-05-03 11:46:11 +0100

    Added more specific info into the blockchain display
    Signed-off-by: Erin Hughes <Erin.Hughes@ibm.com>

--- packages/letters-of-credit/src/components/BlockChainDisplay/BlockChainDisplay.js
+++ packages/letters-of-credit/src/components/BlockChainDisplay/BlockChainDisplay.js
@@ -5,9 +5,50 @@ import Block from '../../components/Block/Block.js';
 class BlockChainDisplay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      transactions: []
-    }
+  }
+
+  getSpecificInfo() {
+    let transactions = [];
+    this.props.transactions.map((tx, index) => {
+      let transaction = {
+        name: "",
+        timestamp: tx.transactionTimestamp
+      };
+
+      if (tx.transactionType === 'org.acme.loc.InitialApplication') {
+        transaction.name = 'Created by Alice';
+      } else if (tx.transactionType === 'org.acme.loc.Approve') {
+        switch (index) {
+          case 1: 
+            transaction.name = 'Approved by Alice'; 
+            break;
+          case 2:
+            transaction.name = 'Approved by Matías';
+            break;
+          case 3:
+            transaction.name = 'Approved by Ella';
+            break;
+          case 4:
+            transaction.name = 'Approved by Bob';
+            break;
+          default: 
+            transaction.name = 'Approved';
+            break;
+        }
+      } else if (tx.transactionType === 'org.acme.loc.ShipProduct') {
+        transaction.name = 'Shipped by Bob';
+      } else if (tx.transactionType === 'org.acme.loc.ReceiveProduct') {
+        transaction.name = 'Received by Alice';
+      } else if (tx.transactionType === 'org.acme.loc.ReadyForPayment') {
+        transaction.name = 'Paid by Matías';
+      } else if (tx.transactionType === 'org.acme.loc.Close') {
+        transaction.name = 'Closed by Ella';
+      } else {
+        transaction.name = 'Rejected';
+      }
+      transactions.push(transaction);
+    });
+    return transactions;
   }
 
   addLeadingZero(number) {
@@ -15,13 +56,13 @@ class BlockChainDisplay extends React.Component {
   }
 
   render() {
-    let transactions = this.props.transactions;
+    let transactions = this.getSpecificInfo();
     let blocks = [];
     if(transactions.length) {
       for (let i = transactions.length; i > 0; i--) {
-        let name = transactions[i-1].transactionType.split(".")[3];
+        let name = transactions[i-1].name;
         let blockNumber = this.addLeadingZero(i);
-        let dateTime = new Date(transactions[i-1].transactionTimestamp);
+        let dateTime = transactions[i-1].timestamp;
         let date = dateTime.getFullYear() + '-' + this.addLeadingZero(dateTime.getMonth()+1) + '-' + this.addLeadingZero(dateTime.getDate());
         let time = dateTime.toTimeString().split(' ')[0];
         blocks.push(<Block transactionDetails={name} date={date} time={time} number={blockNumber}/>);
